5c6a8e6ab449fbeb28e55bb3dead11a612f2a667
ajeigbepaul <pdave4krist@yahoo.com>
2024-08-18 16:26:00 +0100

    integrated mentions on the comment on the home page

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,12 +1,14 @@
 "use client";
 import Loader from "@/components/Loader";
-import { getClerkUsers } from "@/lib/actions/user.action";
+import { getClerkUsers, getDocumentUsers } from "@/lib/actions/user.action";
+import { useUser } from "@clerk/nextjs";
 import {
   LiveblocksProvider,
   ClientSideSuspense,
 } from "@liveblocks/react/suspense";
 import React, { ReactNode } from "react";
 const Provider = ({ children }: { children: ReactNode }) => {
+  const { user: clerkUser } = useUser();
   return (
     <LiveblocksProvider
       authEndpoint="/api/liveblocks-auth"
@@ -14,6 +16,15 @@ const Provider = ({ children }: { children: ReactNode }) => {
         const users = getClerkUsers({ userIds });
         console.log("Resolved Users:", users);
         return users;
+      }}
+      resolveMentionSuggestions={async ({ text, roomId }) => {
+        const roomUsers = await getDocumentUsers({
+          roomId,
+          currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+          text,
+        })
+
+        return roomUsers;
       }}
     >
       <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
